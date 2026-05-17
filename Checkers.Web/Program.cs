09e3b34e98ccef65e061911e13899d541efb7d00
ASis <asisyagin@rfgf.ru>
2026-05-17 19:16:57 +0300

    Добавьте файлы проекта.

---
+++ Checkers.Web/Program.cs
@@ -0,0 +1,16 @@
+namespace Checkers.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.Run();
+
+        }
+    }
+}
